fdda2398da158f71e205546a71f57d71b03a4a26
Vasan R <contact.vasan04@gmail.com>
2024-02-29 20:47:04 +0530

    1st commit

---
+++ codes/llm-with-gradio/basic-nlp-tasks/text.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import gradio as gr
+def image_to_base64_str(pil_image):
+    byte_arr = io.BytesIO()
+    pil_image.save(byte_arr, format='PNG')
+    byte_arr = byte_arr.getvalue()
+    return str(base64.b64encode(byte_arr).decode('utf-8'))
+
+def captioner(image):
+    base64_image = image_to_base64_str(image)
+    result = get_completion(base64_image)
+    return result[0]['generated_text']
+
+gr.close_all()
+demo = gr.Interface(fn=captioner,
+                    inputs=[gr.Image(label="Upload image", type="pil")],
+                    outputs=[gr.Textbox(label="Caption")],
+                    title="Image Captioning with BLIP",
+                    description="Caption any image using the BLIP model",
+                    allow_flagging="never",
+                    examples=["https://free-images.com/lg/9cb8/sunset_sundown_da_nang.jpg", "https://free-images.com/lg/dd2c/port_au_prince_haiti.jpg"])
+
+demo.launch(share=True, server_port=int(os.environ['PORT1']))
